3c1e8d79e6c02bf0b56a8220b8e502aa1a117fc4
sakethramanujam <ramanujamsamavedam@hotmail.com>
2019-11-19 11:43:41 +0530

    add node & action to repo

---
+++ node/src/App.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Table from "./Table";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      buttonClicked: "",
+      assignments: [],
+      grades: {}
+    };
+
+    this.handleButtonClicked = this.handleButtonClicked.bind(this);
+    this.addAssignment = this.addAssignment.bind(this);
+    this.addGrade = this.addGrade.bind(this);
+  }
+
+  handleButtonClicked(buttonName) {
+    this.setState({
+      buttonClicked: buttonName
+    });
+  }
+
+  addAssignment(assignmentName) {
+    this.setState({
+      assignments: this.state.assignments.concat(assignmentName)
+    });
+  }
+
+  addGrade(assignment, student, score) {
+    let grades = this.state.grades;
+    let assignmentName = assignment;
+    let studentName = student;
+    if (!(assignment in grades)) {
+      grades[assignmentName] = {};
+    }
+    grades[assignmentName][studentName] = score;
+    this.setState({ grades: grades });
+  }
+
+  render() {
+    let tabChoice = <div />;
+
+    return (
+      <div>
+        <div className="Box Box--spacious f4">
+          <div className="Box-header">
+          </div>
+        </div>
+        <nav className="UnderlineNav d-flex flex-justify-center">
+          <div className="UnderlineNav-body pt-6">
+            <button
+              className="btn btn-primary"
+              onClick={() => this.handleButtonClicked("assignments")}
+            >
+              Assignments
+            </button>
+            <button
+              className="btn btn-primary"
+              onClick={() => this.handleButtonClicked("students")}
+            >
+              Students
+            </button>
+            <button
+              className="btn btn-primary"
+              onClick={() => this.handleButtonClicked("grades")}
+            >
+              Grades
+            </button>
+          </div>
+        </nav>
+        {tabChoice}
+      </div>
+    );
+  }
+}
+
+export default App;
